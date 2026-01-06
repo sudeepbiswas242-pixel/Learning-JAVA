1b8dcc47763bec1b0fc12f7bb507477538d48ad4
Sudeep Biswas <sudeepbiswas242@gmail.com>
2026-01-06 23:08:22 +0530

    Add Java programs for pyramid patterns

---
+++ src/LeftHalfPyramid.java
@@ -0,0 +1,8 @@
+public class LeftHalfPyramid {
+    static void main(String[] args) {
+        System.out.println("       *");
+        System.out.println("     * *");
+        System.out.println("   * * *");
+        System.out.println(" * * * *");
+    }
+}
---
+++ src/RightHalfPyramid.java
@@ -0,0 +1,10 @@
+public class RightHalfPyramid
+{
+    static void main(String[] args) {
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * * ");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+}
