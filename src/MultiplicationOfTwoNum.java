116990c5386b40026ceffc9144a545cd740b35be
Sudeep Biswas <sudeepbiswas242@gmail.com>
2026-01-12 23:06:12 +0530

    add MultiplicationOfTwoNum program

---
+++ src/MultiplicationOfTwoNum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MultiplicationOfTwoNum {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("enter your first  num: ");
+            int firstnum = input.nextInt();
+            System.out.print(" enter your second num: ");
+            int secondnum = input.nextInt();
+            int mul = firstnum * secondnum;
+            System.out.println("your answer is : " + mul);
+
+        }
+
+    }
+
