aec1c7d666a5add3a591220d5348d1fc38286664
Sudeep Biswas <sudeepbiswas242@gmail.com>
2026-01-12 22:42:52 +0530

    add SubOfTwoNum program

--- src/SubOfTwoNum.java
+++ src/SubOfTwoNum.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class SubOfTwoNum {
+public class SubOfTwoNum{
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("enter your first  num: ");
+    int firstnum = input.nextInt();
+    System.out.print(" enter your second num: ");
+    int secondnum = input.nextInt();
+    int sub = firstnum- secondnum;
+    System.out.println("your answer is : " + sub);
+
+}
+
 }
