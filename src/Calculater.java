67dea5e4d391afd63454f498c0c307180d745b36
Sudeep Biswas <sudeepbiswas242@gmail.com>
2026-01-14 22:25:32 +0530

    add Calculater program

---
+++ src/Calculater.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Calculater {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to our Calculater");
+        System.out.print("enter your first num: ");
+        int firstnum = input.nextInt();
+        System.out.print("enter your second num: ");
+        int secondnum = input.nextInt();
+        int sum = firstnum + secondnum ;
+        int sub = firstnum - secondnum;
+        int mul = firstnum * secondnum;
+        int div = firstnum/secondnum;
+        int modu= firstnum%secondnum;
+        System.out.println(" sum=  " + sum);
+        System.out.println(" sub= " +sub);
+        System.out.println(" mul ="+ mul);
+        System.out.println(" div =" + div);
+        System.out.println(" modu= "+ modu);
+
+
+    }
+}
