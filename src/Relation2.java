a5f8fc34a10ff14bf74d81e8f926361b9a99d49f
Sudeep Biswas <sudeepbiswas242@gmail.com>
2026-01-19 23:00:23 +0530

    add Relation2  program

---
+++ src/Relation2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Relation2 {
+    static void main(String[] args) {
+       boolean IsSeniorCitizen = true;
+       boolean IsAdult= false;
+
+       if (IsSeniorCitizen){
+           System.out.println("Hello SeniorCitizen");
+       }
+       else {
+           if(IsAdult) {
+            System.out.println("Hello Adult");}
+            else {
+                System.out.println("Hello child");
+            }
+        }
+
+    }
+}
