71494db7810887a780986364db09e79a1e2d74d9
Sudeep Biswas <sudeepbiswas242@gmail.com>
2026-01-20 22:12:35 +0530

    add Chack is Positive Negative Zero program

---
+++ src/ChackNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ChackNumber {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Number Chack Calculater");
+        System.out.print("Enter your number: ");
+        int Number = input.nextInt();
+
+
+        if(Number > 0){
+            System.out.println("It is a Positive Number ");
+        }
+        else if(Number==0){
+            System.out.println("Your number is Zero ");
+        }
+        else {
+            System.out.println("It is a Negative Number");
+        }
+    }
+}
