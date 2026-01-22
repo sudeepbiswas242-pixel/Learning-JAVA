17b734900797ea95f6664af291e301df4eeb7d9a
Sudeep Biswas <sudeepbiswas242@gmail.com>
2026-01-22 23:19:30 +0530

    add AgeGroup calculater Program

---
+++ src/AgeGroup.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AgeGroup {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to age Group Calculator ");
+        System.out.print("Enter Your age: ");
+        int age = input.nextInt();
+
+        if(age <= 13){
+            System.out.println("You are a children");
+        }
+        else if (age<=20){
+            System.out.println("You are teenager");
+        }
+        else if(age<=60){
+            System.out.println("You are Adult");
+        }
+        else {
+            System.out.println("you are Senior");
+        }
+    }
+}
