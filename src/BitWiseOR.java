808b4ef66ec3bcaa563f48383428f60e3279f34c
Sudeep Biswas <sudeepbiswas242@gmail.com>
2026-01-24 21:41:13 +0530

    add BitwiseOR calculate Program

---
+++ src/BitWiseOR.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BitWiseOR {
+
+        static void main() {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Welcome to Bite Wise operater calculater For OR");
+            System.out.print("Enter Your Number 1: ");
+            int Num1 = input.nextInt();
+            System.out.print("Enter Your Number 2 : ");
+            int Num2 = input.nextInt();
+
+            int Number  = Num1 | Num2;
+            System.out.println("Your Answer is : "+ Number);
+        }
+    }
+
