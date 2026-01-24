2e56c0d4ef623068ab869de28f163082d2752913
Sudeep Biswas <sudeepbiswas242@gmail.com>
2026-01-24 21:33:22 +0530

    add BitewiseAnd calculate Program

---
+++ src/BitewiseAnd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BitewiseAnd {
+    static void main() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Bite Wise operater calculater For AND");
+        System.out.print("Enter Your Number 1: ");
+        int Num1 = input.nextInt();
+        System.out.print("Enter Your Number 2 : ");
+        int Num2 = input.nextInt();
+
+        int Number  = Num1 & Num2;
+        System.out.println("Your Answer is : "+ Number);
+    }
+}
