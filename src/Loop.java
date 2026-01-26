4bfd8f4928aaab3e85ba5f056ec7d6d68f5d5ff2
Sudeep Biswas <sudeepbiswas242@gmail.com>
2026-01-26 21:47:32 +0530

    add Loop Basic Program

---
+++ src/Loop.java
@@ -0,0 +1,13 @@
+public class Loop {
+    static void main(String[] args) {
+        int num=1;
+
+        while (num<=10){
+            System.out.println(num);
+            num=num+1;
+        }
+
+    }
+
+
+}
