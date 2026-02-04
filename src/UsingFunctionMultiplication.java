e449e53d636df6f7000c5246f5b003c3f0460326
Sudeep Biswas <sudeepbiswas242@gmail.com>
2026-02-04 23:21:04 +0530

    add  multiplication tow number using function  Program

---
+++ src/UsingFunctionMultiplication.java
@@ -0,0 +1,14 @@
+public class UsingFunctionMultiplication {
+
+    // Function
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+
+        int result = multiply(5, 4);   // Function call
+
+        System.out.println("Answer = " + result);
+    }
+}
