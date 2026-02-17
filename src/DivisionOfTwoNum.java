a604f177a5f74770ae1f67d55c5ec6f09021afcd
sudeepbiswas242-pixel <sudeepbiswas242@gmail.com>
2026-02-17 23:20:07 +0530

    Refactor firstnum declaration formatting
    Fixed formatting of variable declaration for firstnum.

--- src/DivisionOfTwoNum.java
+++ src/DivisionOfTwoNum.java
@@ -4,7 +4,7 @@ public class DivisionOfTwoNum {
     public static void main( String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print(" enter your first num: ");
-        int firstnum = input.nextInt();
+        int firstnum =  input.nextInt();
         System.out.print("enter your second num: ");
         int secondnum = input.nextInt();
         int divison = firstnum/secondnum;
