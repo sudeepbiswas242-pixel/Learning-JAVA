10a3a4b2be8ee18b9eed6a7aa7b0a703096e5f41
sudeepbiswas242-pixel <sudeepbiswas242@gmail.com>
2026-02-28 23:32:20 +0530

    Update BitWiseXOR.java

--- src/BitWiseXOR.java
+++ src/BitWiseXOR.java
@@ -3,7 +3,7 @@ public class BitWiseXOR {
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome  to XOR Calculator");
-        System.out.print("Enter Your First Number : ");
+        System.out.print("Enter  Your First Number : ");
         int num1 = input.nextInt();
         System.out .print("Enter your second Number : ");
         int num2 =input.nextInt();
