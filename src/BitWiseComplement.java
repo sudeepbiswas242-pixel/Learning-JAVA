0f26d3ad5f6c4d3c8e6656e14a245a6fd3bd5ab7
sudeepbiswas242-pixel <sudeepbiswas242@gmail.com>
2026-03-02 23:20:38 +0530

    Update BitWiseComplement.java

--- src/BitWiseComplement.java
+++ src/BitWiseComplement.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class BitWiseComplement {
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to Compliment Calculator");
+        System.out.println("Welcome  to Compliment Calculator");
         System.out.print("Enter  Your  Number : ");
         int num1 = input.nextInt();
 
