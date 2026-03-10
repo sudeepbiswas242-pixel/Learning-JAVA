6526859a981ba9aebfef310fab8830f0aa8bdc0e
sudeepbiswas242-pixel <sudeepbiswas242@gmail.com>
2026-03-10 23:36:43 +0530

    Fix typo in prompt for height input

--- src/AreaOfTriangle.java
+++ src/AreaOfTriangle.java
@@ -6,7 +6,7 @@ public class AreaOfTriangle {
         System.out.println(" Calculate  Area of Triangle");
         System.out.print(" Enter the value of Breath : ");
         double Breath = input.nextDouble();
-        System.out.print(" Enter the value of height : ");
+        System.out.print(" Enter the value of  height : ");
         double height = input.nextDouble();
         double area = 0.5*Breath*height; // 1/2 = 0.5
         System.out.println(" your answer is : "+ area+"cm");
