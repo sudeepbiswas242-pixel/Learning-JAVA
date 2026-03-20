d041ac4b8b865bbff963d1d0c55bac7d9668d552
sudeepbiswas242-pixel <sudeepbiswas242@gmail.com>
2026-03-20 22:09:36 +0530

    Update EvenOdd.java

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -7,7 +7,7 @@ public class EvenOdd {
         if (num % 2 == 0) {
             System.out.println("Even  number");
         } else {
-            System.out.println("Odd number");
+            System.out.println("Odd  number");
         }
     }
 }
