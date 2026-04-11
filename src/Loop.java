892a0e0a401c6e435858fc6aeb4d6b622f4f2d7e
sudeepbiswas242-pixel <sudeepbiswas242@gmail.com>
2026-04-11 22:35:28 +0530

    Update Loop.java

--- src/Loop.java
+++ src/Loop.java
@@ -2,7 +2,7 @@ public class Loop {
     static void main(String[] args) {
         int num=1;
 
-        while (num<=10){
+        while (num<=10)b{
             System.out.println(num);
             num=num+1;
         }
