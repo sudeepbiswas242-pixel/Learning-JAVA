f7ef26baf49d93f3f2c1d1ac7e736848c54631e3
sudeepbiswas242-pixel <sudeepbiswas242@gmail.com>
2026-04-12 23:07:47 +0530

    Update RightShift.java

--- src/RightShift.java
+++ src/RightShift.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class RightShift {
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to Right Shift Calculator");
+        System.out.println ("Welcome to Right Shift Calculator");
         System.out.print("Enter Your  Number : ");
         int num1 = input.nextInt();
 
