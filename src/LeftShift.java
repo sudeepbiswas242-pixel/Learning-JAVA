55c0dc84f56498baa0a80e007b5a7bacde03ff2c
sudeepbiswas242-pixel <sudeepbiswas242@gmail.com>
2026-04-17 22:37:06 +0530

    Update LeftShift.java

--- src/LeftShift.java
+++ src/LeftShift.java
@@ -4,7 +4,7 @@ public class LeftShift {
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to left Shift Calculator");
-        System.out.print("Enter Your  Number : ");
+        System.out.print ("Enter Your  Number : ");
         int num1 = input.nextInt();
 
 
